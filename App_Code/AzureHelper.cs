30f4bf40d888abd42fc683fe5bed13777e99d4c7
huangcd <huangcd.thu@gmail.com>
2013-06-17 10:49:02 +0800

    Init

---
+++ App_Code/AzureHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+/// <summary>
+/// Summary description for AzureHelper
+/// </summary>
+public static class AzureHelper
+{
+    private readonly static CloudStorageAccount account = CloudStorageAccount.Parse(
+            CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+    public static CloudStorageAccount Account
+    {
+        get { return account; }
+    }
+
+    public static CloudTableClient TableClient
+    {
+        get { return account.CreateCloudTableClient(); }
+    }
+}
---
+++ venueList.js
@@ -0,0 +1,100 @@
+[
+    {
+        "Center": {
+            "Altitude": 0,
+            "AltitudeReference": 0,
+            "Latitude": 39.979257994507798,
+            "Longitude": 116.304071868238
+        },
+        "DefaultLevel": 12,
+        "DefaultZoom": 20,
+        "FloorHeader": "Floor",
+        "FloorList": [1, 2, 3, 7, 8, 9, 10, 11, 12, 13, 14],
+        "ReadableName": "BJW 2",
+        "VenueId": "hcl-microsoftbjw2"
+    },
+    {
+        "Center": {
+            "Altitude": 0,
+            "AltitudeReference": 0,
+            "Latitude": 39.9782737419746,
+            "Longitude": 116.304073995188
+        },
+        "DefaultLevel": 2,
+        "DefaultZoom": 20,
+        "FloorHeader": "Floor",
+        "FloorList": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+        "ReadableName": "BJW 1",
+        "VenueId": "hcl-microsoftbjw1"
+    },
+    {
+        "Center": {
+            "Altitude": 0,
+            "AltitudeReference": 0,
+            "Latitude": 47.642854999999997,
+            "Longitude": -122.125466
+        },
+        "DefaultLevel": 1,
+        "DefaultZoom": 20,
+        "FloorHeader": "Level",
+        "FloorList": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        "ReadableName": "Building 33",
+        "VenueId": "bingmapsteam-msftbuilding33"
+    },
+    {
+        "Center": {
+            "Altitude": 0,
+            "AltitudeReference": 0,
+            "Latitude": 47.643977999999997,
+            "Longitude": -122.125584
+        },
+        "DefaultLevel": 1,
+        "DefaultZoom": 20,
+        "FloorHeader": "Level",
+        "FloorList": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        "ReadableName": "Building 34",
+        "VenueId": "bingmapsteam-msftbuilding34"
+    },
+    {
+        "Center": {
+            "Altitude": 0,
+            "AltitudeReference": 0,
+            "Latitude": 47.642231000000002,
+            "Longitude": -122.14197900000001
+        },
+        "DefaultLevel": 2,
+        "DefaultZoom": 19,
+        "FloorHeader": "Level",
+        "FloorList": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        "ReadableName": "Building 99",
+        "VenueId": "bingmapsteam-msftbuilding99"
+    },
+    {
+        "Center": {
+            "Altitude": 0,
+            "AltitudeReference": 0,
+            "Latitude": 47.61571,
+            "Longitude": -122.204038
+        },
+        "DefaultLevel": 1,
+        "DefaultZoom": 17.300000000000001,
+        "FloorHeader": "Level",
+        "FloorList": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        "ReadableName": "Bellevue Square Mall",
+        "VenueId": "bingmapsteam-bellevuesquare"
+    },
+    {
+        "Center": {
+            "Altitude": 0,
+            "AltitudeReference": 0,
+            "Latitude": 47.643428999999998,
+            "Longitude": -122.14100999999999
+        },
+        "DefaultLevel": 1,
+        "DefaultZoom": 19,
+        "FloorHeader": "Level",
+        "FloorList": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        "ReadableName": "Studio H",
+        "VenueId": "hcl-studioh"
+    }
+]
