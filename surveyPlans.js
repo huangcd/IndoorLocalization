cce5656efe985b5db97e323d4fbdf804af60ca8f
huangcd <huangcd.thu@gmail.com>
2013-06-20 10:37:56 +0800

    format survey plans
    Signed-off-by: huangcd <huangcd.thu@gmail.com>

--- surveyPlans.js
+++ surveyPlans.js
@@ -1,1 +1,159 @@
-[{"ID":"48968232-682d-4f9a-9c59-dc62e72c15c4","Level":12,"Locations":[{"Altitude":0,"AltitudeReference":0,"Latitude":39.9796438449379,"Longitude":116.304129022696},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795929761519,"Longitude":116.304126340487},{"Altitude":0,"AltitudeReference":0,"Latitude":39.979589893194,"Longitude":116.304243016579},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9794316344993,"Longitude":116.304237652161},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9794264962239,"Longitude":116.304275203087},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9793838485228,"Longitude":116.304287273027},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9793745996202,"Longitude":116.303937244751},{"Altitude":0,"AltitudeReference":0,"Latitude":39.979596572936,"Longitude":116.303932550885},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795960591097,"Longitude":116.304607126452},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9793715166524,"Longitude":116.304605785348},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9793740857922,"Longitude":116.304303366282},{"Altitude":0,"AltitudeReference":0,"Latitude":39.979589893194,"Longitude":116.30430001352}],"RoadWidth":3,"VenueId":"hcl-microsoftbjw2"},{"ID":"bc3ef17d-109d-4e40-8e53-39ceabb1a23f","Level":12,"Locations":[{"Altitude":0,"AltitudeReference":0,"Latitude":39.9796413098525,"Longitude":116.304127926206},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795909548909,"Longitude":116.304126585101},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795935240225,"Longitude":116.30459328947},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9793787442881,"Longitude":116.304594630574},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9793761751484,"Longitude":116.303942853784},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795883857593,"Longitude":116.303941512679},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795924963699,"Longitude":116.304125914549}],"RoadWidth":3,"VenueId":"hcl-microsoftbjw2"},{"ID":"e807b1b6-c4ba-48cb-a3d5-05b667826157","Level":12,"Locations":[{"Altitude":0,"AltitudeReference":0,"Latitude":39.9796418236784,"Longitude":116.304127255654},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795894134119,"Longitude":116.304125243997},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795899272383,"Longitude":116.304234544014},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9794306408887,"Longitude":116.304233873462},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9794301270611,"Longitude":116.304264718866},{"Altitude":0,"AltitudeReference":0,"Latitude":39.979379258116,"Longitude":116.304280141567},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9793761751484,"Longitude":116.303940842127},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795863304538,"Longitude":116.303941512679},{"Altitude":0,"AltitudeReference":0,"Latitude":39.9795904410646,"Longitude":116.304123902892}],"RoadWidth":3,"VenueId":"hcl-microsoftbjw2"}]
+[ { "ID" : "48968232-682d-4f9a-9c59-dc62e72c15c4",
+    "Level" : 12,
+    "Locations" : [ { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979643844937897,
+          "Longitude" : 116.304129022696
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979592976151899,
+          "Longitude" : 116.304126340487
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979589893194003,
+          "Longitude" : 116.30424301657899
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979431634499299,
+          "Longitude" : 116.30423765216101
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979426496223901,
+          "Longitude" : 116.304275203087
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979383848522801,
+          "Longitude" : 116.304287273027
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979374599620201,
+          "Longitude" : 116.303937244751
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979596572936003,
+          "Longitude" : 116.30393255088499
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979596059109703,
+          "Longitude" : 116.304607126452
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.9793715166524,
+          "Longitude" : 116.304605785348
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979374085792202,
+          "Longitude" : 116.304303366282
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979589893194003,
+          "Longitude" : 116.30430001352001
+        }
+      ],
+    "RoadWidth" : 3,
+    "VenueId" : "hcl-microsoftbjw2"
+  },
+  { "ID" : "bc3ef17d-109d-4e40-8e53-39ceabb1a23f",
+    "Level" : 12,
+    "Locations" : [ { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979641309852497,
+          "Longitude" : 116.30412792620599
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979590954890902,
+          "Longitude" : 116.304126585101
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979593524022498,
+          "Longitude" : 116.30459328947001
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979378744288098,
+          "Longitude" : 116.304594630574
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979376175148403,
+          "Longitude" : 116.30394285378399
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.9795883857593,
+          "Longitude" : 116.303941512679
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979592496369897,
+          "Longitude" : 116.304125914549
+        }
+      ],
+    "RoadWidth" : 3,
+    "VenueId" : "hcl-microsoftbjw2"
+  },
+  { "ID" : "e807b1b6-c4ba-48cb-a3d5-05b667826157",
+    "Level" : 12,
+    "Locations" : [ { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979641823678399,
+          "Longitude" : 116.304127255654
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979589413411901,
+          "Longitude" : 116.30412524399701
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979589927238301,
+          "Longitude" : 116.30423454401399
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979430640888701,
+          "Longitude" : 116.304233873462
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.9794301270611,
+          "Longitude" : 116.30426471886599
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979379258115998,
+          "Longitude" : 116.30428014156701
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979376175148403,
+          "Longitude" : 116.30394084212701
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979586330453799,
+          "Longitude" : 116.303941512679
+        },
+        { "Altitude" : 0,
+          "AltitudeReference" : 0,
+          "Latitude" : 39.979590441064602,
+          "Longitude" : 116.304123902892
+        }
+      ],
+    "RoadWidth" : 3,
+    "VenueId" : "hcl-microsoftbjw2"
+  }
+]
